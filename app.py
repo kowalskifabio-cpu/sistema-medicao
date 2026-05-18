8e8d80330367bcc7f89422b892709350b3f4fb48
kowalskifabio-cpu <kowalski.fabio@gmail.com>
2026-05-18 11:02:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,7 @@ import re
 from datetime import datetime
 
 # --- 1. CONFIGURAÇÕES ---
-URL_DO_APPS_SCRIPT = "https://script.google.com/macros/s/AKfycbzgnCmVZURdpN6LF54lYWyNSeVLvV36FQwB9DMSa2_lEF8Nm-lsvYzv_qmqibe-hcRp/exec"
-TOKEN = "CHAVE_SEGURA_123"
+
 
 st.set_page_config(page_title="Gestão de Medições Pro", layout="wide")
 
